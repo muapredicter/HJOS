bbf28208270c1985b7bc0b66a8621486b893c0de
muapredicter <2816137428@qq.com>
2024-08-07 19:47:23 +0800

    chapter8/d

---
+++ chatper8/d/kernel/main.c
@@ -0,0 +1,10 @@
+#include "print.h"
+#include "init.h"
+#include "debug.h"
+int main(void)
+{
+   put_str("I am kernel\n");
+   init_all();
+   while (1);
+   return 0;
+}
---
+++ chatper8/d/kernel/memory.h
@@ -0,0 +1,15 @@
+#ifndef __KERNEL_MEMORY_H
+#define __KERNEL_MEMORY_H
+#include "stdint.h"
+#include "../lib/kernel/bitmap.h"
+
+extern struct pool kernel_pool, user_pool;
+void mem_init(void);
+
+//核心数据结构，虚拟内存池，有一个位图与其管理的起始虚拟地址
+struct virtual_addr {
+   struct bitmap vaddr_bitmap;      // 虚拟地址用到的位图结构 
+   uint32_t vaddr_start;            // 虚拟地址起始地址
+};
+
+#endif
