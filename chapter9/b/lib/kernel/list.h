f3e8470e70b998017d0da21a47e2407d2211c8fe
muapredicter <2816137428@qq.com>
2024-08-14 14:39:24 +0800

    change list.h

--- chapter9/b/lib/kernel/list.h
+++ chapter9/b/lib/kernel/list.h
@@ -25,15 +25,16 @@ struct list
 #define member_to_entry(struct_type, member_name, member_ptr) (struct_type *)((int)member_ptr - offset(struct_type, member_name))
 
 typedef bool(function)(struct list_elem *, int arg);
-void list_init(struct list *list);
-void list_insert(struct list *link, struct list *new_link);
-void list_push(struct list *list, struct list *new_link);
-void list_append(struct list *list, struct list *new_link);
-void list_remove(struct list *link);
-struct list *list_pop(struct list *list);
-int list_find(struct list *list, struct list *link);
-int list_empty(struct list *list);
-uint32_t list_len(struct list *list);
-struct list *list_traversal(struct list *list, function func, int arg);
+void list_init (struct list*);
+void list_insert_before(struct list_elem* before, struct list_elem* elem);
+void list_push(struct list* plist, struct list_elem* elem);
+void list_iterate(struct list* plist);
+void list_append(struct list* plist, struct list_elem* elem);  
+void list_remove(struct list_elem* pelem);
+struct list_elem* list_pop(struct list* plist);
+bool list_empty(struct list* plist);
+uint32_t list_len(struct list* plist);
+struct list_elem* list_traversal(struct list* plist, function func, int arg);
+bool elem_find(struct list* plist, struct list_elem* obj_elem);
 
 #endif
